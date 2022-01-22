8ad9d6c4870068af8bd58ca009f00def1db4c20e
Amooraj <ambreen.mooraj@gmail.com>
2022-01-21 21:52:02 -0500

    added dollar sign

--- src/Menu.js
+++ src/Menu.js
@@ -10,7 +10,8 @@ return <div className="section-center">
    <div className="item-info">
     <header>
      <h4>{title}</h4>
-     <h4 className="price">{price}</h4>
+     <h4 className="price">
+      ${price}</h4>
     </header>
     <p className="item-text">{desc}</p>
    </div>
